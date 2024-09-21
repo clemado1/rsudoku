a3feb74fd9ec39f942d04afdd6b76f846664a7ef
clemado1 <clemado1@gmail.com>
2024-09-22 00:58:03 +0900

    add: ads.txt and copy paste plugin

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -23,6 +24,11 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({
             filename: '[name].css',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'ads.txt', to: 'ads.txt' }
+            ],
         }),
     ],
     devServer: {
